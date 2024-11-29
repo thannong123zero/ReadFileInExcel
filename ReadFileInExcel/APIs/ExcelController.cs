7372ccf1ea6653c689f7fc8c6e4ca6f615d4a743
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-11-29 11:33:30 +0700

    Add project files.

---
+++ ReadFileInExcel/APIs/ExcelController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReadFileInExcel.APIs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExcelController : ControllerBase
+    {
+    }
+}
---
+++ ReadFileInExcel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReadFileInExcel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
